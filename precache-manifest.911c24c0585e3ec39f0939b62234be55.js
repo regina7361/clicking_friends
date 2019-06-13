2f81911b9e27e3e110ad7ab96adf2a8b27a43491
regina7361 <regina7361@gmail.com>
2019-06-12 21:24:57 -0700

    Updates

---
+++ precache-manifest.911c24c0585e3ec39f0939b62234be55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19334fdb49a6f96d87ff894768ea76a5",
+    "url": "/clicking_friends/index.html"
+  },
+  {
+    "revision": "443225ddf61309139c81",
+    "url": "/clicking_friends/static/css/main.b38d1168.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicking_friends/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "443225ddf61309139c81",
+    "url": "/clicking_friends/static/js/main.678d876c.chunk.js"
+  },
+  {
+    "revision": "d1082b8e08b2e6e6b536",
+    "url": "/clicking_friends/static/js/runtime~main.d3dc2f6b.js"
+  }
+]);
